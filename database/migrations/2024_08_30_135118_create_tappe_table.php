1f3cb3f4c7913bd6266b8a450d1045d4e96c1966
NeilMagon <neil.magon97@gmail.com>
2024-08-30 15:54:30 +0200

    feat: create migration

---
+++ database/migrations/2024_08_30_135118_create_tappe_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tappe', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('giornata_id')->constrained()->onDelete('cascade');
+            $table->string('titolo');
+            $table->text('descrizione')->nullable();
+            $table->string('immagine')->nullable();
+            $table->decimal('latitudine', 10, 7);
+            $table->decimal('longitudine', 10, 7);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tappe');
+    }
+};
